f669c163f11bf5f464d3e390f514b2edc0a3b153
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-30 23:41:18 -0300

    feat: get keyboard input

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,12 +14,20 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    //Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -40,7 +48,18 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update(){
-
+        if(keyHandler.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyHandler.downPressed){
+            playerY += playerSpeed;
+        }
+        else if(keyHandler.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyHandler.rightPressed){
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics graphics){
@@ -51,7 +70,7 @@ public class GamePanel extends JPanel implements Runnable {
         graphics2D.setColor(Color.white);
 
         //Draw Rectangle
-        graphics2D.fillRect(100, 100, tileSize, tileSize);
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
 
         //Dispose of this graphics context and release any system resources using it.
         graphics2D.dispose();
