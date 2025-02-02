0920fc32e58996a1a66df8b8e04ef9ba8eb5106c
jeffersonrolino <jefferson.rolino@gmail.com>
2025-02-01 22:32:22 -0300

    feat: create map data with text file

--- src/tiles/TileManager.java
+++ src/tiles/TileManager.java
@@ -4,16 +4,22 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class TileManager {
     GamePanel gamePanel;
     Tile[] tiles;
+    int[][] mapTileNumber;
 
     public TileManager(GamePanel gamePanel){
         this.gamePanel = gamePanel;
         tiles = new Tile[10];
+        mapTileNumber = new int[gamePanel.maxScreenCol][gamePanel.maxScreenRow];
+
         getTileImage();
     }
 
